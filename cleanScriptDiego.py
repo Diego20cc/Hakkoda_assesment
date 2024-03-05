1dedc04a79fa1206fd9a9d77a51b5130731cc55d
Diego20cc <162376564+Diego20cc@users.noreply.github.com>
2024-03-05 16:00:43 -0600

    arreglar un bug
    Arreglar que los encabezados no se encontraban en la primera fila.

--- cleanScriptDiego.py
+++ cleanScriptDiego.py
@@ -20,19 +20,22 @@ def fill_product (datafour):
         if datafour[row][8] == '':
             datafour[row][8] = 'product_' + datafour[row][7]
 
-            
      
 def duplicates(datadup):
     for row in range(0, len(datadup)):
         datadup[row] = ','.join(datadup[row])
-    datadup = set(datadup)#set gerera un conjuno que no puede tener duplicados
+    headers = datadup[0]
+    datadup.pop(0)
+    setdup = set(datadup)#set gerera un conjuno que no puede tener duplicados
+    datadup = list(setdup)
+    datadup.insert(0, headers)
     return datadup
 
     
 fill_client (data_one)
 fill_store (data_one)
 fill_product (data_one)
-data_one = list(duplicates(data_one))
+data_one = duplicates(data_one)
 data_one = "\n".join (data_one)
 
 transactions_clean = open('transactions_clean.csv', 'w')
