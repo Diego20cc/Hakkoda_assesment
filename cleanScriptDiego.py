86a313cecc1fbeafe3ab52b91f6d55cf5f525247
Diego20cc <162376564+Diego20cc@users.noreply.github.com>
2024-03-06 16:04:20 -0600

    Update cleanScriptDiego.py
    Added coments

--- cleanScriptDiego.py
+++ cleanScriptDiego.py
@@ -6,17 +6,17 @@ for row in range(0, len(data_one)):
 data_one.pop()
 
 def fill_client (datatwo):
-    for row in range(0, len(datatwo)):
+    for row in range(0, len(datatwo)):#rellenar nulls con la info de Client_ID
         if datatwo[row][1] == '':
             datatwo[row][1] = 'client_' + datatwo[row][0]
 
 def fill_store (datathree):
-    for row in range(0, len(datathree)):
+    for row in range(0, len(datathree)):#rellenar  nulls con la info de Store_ID
         if datathree[row][5] == '':
             datathree[row][5] = 'store_' + datathree[row][4]
 
 def fill_product (datafour):
-    for row in range(0, len(datafour)):
+    for row in range(0, len(datafour)): #rellenar nulls con la info de Product_ID
         if datafour[row][8] == '':
             datafour[row][8] = 'product_' + datafour[row][7]
 
@@ -38,6 +38,6 @@ fill_product (data_one)
 data_one = duplicates(data_one)
 data_one = "\n".join (data_one)
 
-transactions_clean = open('transactions_clean.csv', 'w')
+transactions_clean = open('transactions_clean.csv', 'w') #recuperar el CVS con las modificaciones.
 transactions_clean.write(data_one)
 transactions_clean.close()
